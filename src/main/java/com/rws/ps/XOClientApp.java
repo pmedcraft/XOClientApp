550196a2f1db41118ff8117147cba5329f045285
Philip Medcraft <pmedcraft@sdl.com>
2023-07-19 13:21:13 +0100

    initial commit

---
+++ src/main/java/com/rws/ps/XOClientApp.java
@@ -0,0 +1,28 @@
+package com.rws.ps;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.List;
+
+/**
+ * @author pmedcraft
+ */
+@SpringBootApplication
+@Slf4j
+public class XOClientApp extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();
+        List<String> arguments = runtimeMxBean.getInputArguments();
+        log.info("retrieving VM arguments");
+        for (String value : arguments) {
+            log.info("argument value:" + value);
+        }
+        SpringApplication.run(XOClientApp.class, args);
+    }
+}
---
+++ src/main/java/com/rws/ps/controller/XOClientController.java
@@ -0,0 +1,80 @@
+package com.rws.ps.controller;
+
+import com.tridion.smarttarget.SmartTargetException;
+import com.tridion.smarttarget.query.Promotion;
+import com.tridion.smarttarget.query.ResultSet;
+import com.tridion.smarttarget.query.builder.*;
+import com.tridion.smarttarget.utils.TcmUri;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.ParseException;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author pmedcraft
+ */
+@Controller
+@Slf4j
+public class XOClientController {
+
+    @GetMapping("/")
+    public String outputForm() {
+        return "promotions-form";
+    }
+
+    @ResponseBody
+    @PostMapping(value = {"/promotions"})
+    public List<Promotion> getPromotions(@RequestParam("pubId") String pubId,
+                                         @RequestParam("pageId") String pageId,
+                                         @RequestParam("region") String region,
+                                         @RequestParam("city") String city,
+                                         @RequestParam("browser") String browser) throws ParseException, SmartTargetException {
+
+        TcmUri publicationUri = new TcmUri(String.format("tcm:0-%s-1", pubId));
+        TcmUri pageUri = new TcmUri(String.format("tcm:%s-%s-64", pubId, pageId));
+
+        QueryBuilder queryBuilder = new QueryBuilder();
+
+        String triggers = "";
+        if (StringUtils.isNotEmpty(city)) {
+            triggers += String.format("&Contact - City=%s", city);
+        }
+        if (StringUtils.isNotEmpty(browser)) {
+            triggers += String.format("&Visitor - Browser=%s", browser);
+        }
+        if (StringUtils.isNotEmpty(triggers))
+            queryBuilder.parseQueryString(triggers);
+
+        queryBuilder
+                .addCriteria(new PublicationCriteria(publicationUri))
+                .addCriteria(new PageCriteria(pageUri));
+
+        if (StringUtils.isNotEmpty(region))
+            queryBuilder.addCriteria(new RegionCriteria(region));
+
+        /*
+        List<String> regions = List.of("Header", "Footer", "Sidebar", "Inset 1", "Inset 2");
+        for (String region : regions) {
+            queryBuilder.addCriteria(new RegionCriteria(region));
+        }
+        */
+
+        ResultSet resultSet = queryBuilder.execute();
+        if (resultSet != null) {
+            log.info(String.format("Number of Promotions: %d", resultSet.getPromotions().size()));
+            return resultSet.getPromotions();
+        }
+
+        return Collections.emptyList();
+    }
+}
